6e32f7c65d23a73de67158fe869588ed693bbd58
aberner6 <annelieberner@gmail.com>
2016-06-13 17:47:49 +0200

    images

--- dcMay/wholeCard.js
+++ dcMay/wholeCard.js
@@ -802,18 +802,27 @@ function goButton(incomingData, imgData){
 		img: "http://pelars.sssup.it/pelars/snapshot/"+thisSession+"/"+(button1[i].time/1000000000000)+"E12"
 		})
 	}
-	// for(i=0; i<button2.length; i++){
-	// 	btnImg2.push({
-	// 		time: button2[i].time,
-	// 		img: "http://pelars.sssup.it/pelars/snapshot/"+thisSession+"/"+(button2[i].time/1000000000000)+"E12"
-	// 	})
-	// }
+	console.log(button2.length+"button2 length");
 	for(i=0; i<button2.length; i++){
+		console.log(button2[i].time)
 		$.getJSON("http://pelars.sssup.it/pelars/snapshot/"+thisSession+"/"+(button2[i].time/1000000000000)+"E12"+"?token="+token, function(json){
-			btnImg2.push(json);
+			btnImg2.push({
+				time: i,
+				data: json
+			});
 		})
 	}
-	console.log(img2Data);
+	// for(i=0; i<btnImg2.length; i++){
+	// 	btnImg2[i].time.sort(d3.ascending) //trying to order based on time first
+	// }
+	// btnImg2_nest = d3.nest()
+	// 	.key(function(d) { 
+	// 		return d.time; 
+	// 	})
+	// 	.sortKeys(d3.ascending)
+	// 	.entries(btnImg2);
+
+	// console.log(img2Data);
 
 	var xSpace = d3.scale.linear()
 		.domain([0, buttonTot+1])
@@ -859,32 +868,40 @@ function goButton(incomingData, imgData){
 		.attr("y", timelineThunderY)
 		.attr("width",iconW)
 		.attr("height",iconW)
-		.on("mouseover", function(d,i){
+		.on("click", function(d,i){
 			var thisData = d3.select(this);
 			thisTime = thisData[0][0].__data__.time;
 			var thisIndex = i;
-
+			var thunderImgW = 200;
+			console.log(thisIndex)
 		    thunder = timeSVG.selectAll(".clip-circ"+thisIndex)
-                .data(btnImg2[thisIndex]) 
-                .attr("x", timeX(thisTime))
+                .data(btnImg2[thisIndex].data) //btnImg2[thisIndex].data or take off data 
+                .attr("id","clip-circ")
+                .attr("x", timeX(thisTime)-thunderImgW/2)
             thunder
                 .enter()
                 .append("image")
                 .attr("class", "clip-circ"+thisIndex)
-                .attr("x", timeX(thisTime))
+                .attr("id","clip-circ")
+                // .attr("class",thisIndex)
+                // .style("clip-path", url(#clipCirc))
+                .attr("x", timeX(thisTime)-thunderImgW/2)
 				.attr("y", timelineThunderY)
-        		.attr("width", timelineImgWidth*4)
+        		.attr("width", thunderImgW)
         		.attr("height", timelineImgHeight*4)
                 .attr("xlink:href", function(d, i) {
                 	// console.log(d);
                 	if(d.view=="workspace"){
                 		return d.data;
+                	} else if (d.view=="people"){
+                		return d.data;
                 	}
                 });
+			thunder.exit();
 		})
-		.on("mouseout", function(d,i){
-			thunder.remove();
-		})
+		// .on("mouseout", function(d,i){
+		// 	thunder.exit().remove();
+		// })
 
 	var iconLine2 = timeSVG.selectAll(".button2L")	
 		.data(button2)
