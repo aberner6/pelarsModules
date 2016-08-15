a44da26218ea7e5ecbc45b3b686817f334152f70
Annelie Berner <annelieberner@Annelies-MacBook-Air.local>
2016-08-15 12:48:26 +0200

    text wrapping

--- dc_v2/ui.js
+++ dc_v2/ui.js
@@ -52,6 +52,7 @@ function setSVG(){
 	  // x.domain([0, d3.max(data, function(d) { return d.value; })]);
 	  data.push(dataIs);
 	  console.log(data);
+	  makeThings(data);
 	})
 }
 function makeThings(data){
@@ -115,16 +116,37 @@ console.log(data);
 	    .attr("r",5)
 	    .attr("fill","pink");
 
+	backR.append("text").attr("class","textie")
+	      .attr("x", rectWidth/4)
+	      .attr("y", rectHeight / 2)
+	      .attr("dy", ".35em")
+	      .text(function(d) { return d.descrip })
+        // New added line to call the function to wrap after a given width
+        .call(wrap, rectWidth-20);
 
-  backR.append("text")
-      .attr("x", rectWidth/4)
-      .attr("y", rectHeight / 2)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name; });
-
-
-
-
+	function wrap(text, width) {
+	  text.each(function() {
+	    var text = d3.select(this),
+	        words = text.text().split(/\s+/).reverse(),
+	        word,
+	        line = [],
+	        lineNumber = 0,
+	        lineHeight = 1.1, // ems
+	        y = text.attr("y"),
+	        dy = parseFloat(text.attr("dy")),
+	        tspan = text.text(null).append("tspan").attr("x", 10).attr("y", y).attr("dy", dy + "em");
+	    while (word = words.pop()) {
+	      line.push(word);
+	      tspan.text(line.join(" "));
+	      if (tspan.node().getComputedTextLength() > width) {
+	        line.pop();
+	        tspan.text(line.join(" "));
+	        line = [word];
+	        tspan = text.append("tspan").attr("x", 10).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+	      }
+	    }
+	  });
+	}
 
 
 
